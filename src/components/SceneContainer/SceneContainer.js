d79f86369dbdf1424345f0e7398d2450395b187c
colbyfayock <fayock@gmail.com>
2020-12-03 08:35:22 -0500

    tweaks

--- src/components/SceneContainer/SceneContainer.js
+++ src/components/SceneContainer/SceneContainer.js
@@ -1,7 +1,21 @@
+import { useRouter } from 'next/router';
+import { useToast } from '@chakra-ui/core';
+import { FaDiscord } from 'react-icons/fa';
+
+import useEvent from '@hooks/useEvent';
+import useChatListener from '@hooks/useChatListener';
+
 import SpaceJelly from '@components/SpaceJelly';
 import Cosmo from '@components/Cosmo';
+import TwitchChatBox from '@components/TwitchChatBox';
 
 import styles from './SceneContainer.module.scss';
+
+const CHAT_COMMANDS = {
+  '!test': 'This is a test response from your chat command 👍',
+};
+
+const ALERT_TIMER = 4000;
 
 export default function SceneContainer({ children, className, ...rest }) {
   let sceneClassName = styles.SceneContainer
@@ -10,6 +24,46 @@ export default function SceneContainer({ children, className, ...rest }) {
     sceneClassName = `${sceneClassName} ${className}`;
   }
 
+  const { connectListener, messages } = useChatListener({
+    channel: process.env.CHANNEL,
+    commands: CHAT_COMMANDS,
+  });
+
+  const event = useEvent({
+    disappear: ALERT_TIMER,
+    channels: ['events'],
+  });
+
+  const [current, setCurrent] = React.useState(event);
+  const [stale, setStale] = React.useState(false);
+  const timeout = React.useRef();
+  const toast = useToast();
+  const router = useRouter();
+  const urlRef = React.useRef(router.pathname);
+
+  React.useEffect(() => {
+    connectListener('on');
+    return () => {
+      if (urlRef.current !== router.pathname) {
+        connectListener('off');
+      }
+    };
+  }, [router.pathname]);
+
+
+  React.useEffect(() => {
+    if (current !== event) {
+      clearTimeout(timeout.current);
+
+      setStale(false);
+      setCurrent(event);
+
+      timeout.current = setTimeout(() => {
+        setStale(true);
+      }, ALERT_TIMER);
+    }
+  }, [event, current, stale, setStale, setCurrent]);
+
   return (
     <div className={sceneClassName} {...rest}>
       <div className={styles.SceneContainerBody}>
@@ -17,8 +71,26 @@ export default function SceneContainer({ children, className, ...rest }) {
       </div>
       <div className={styles.SceneContainerFooter}>
         <SpaceJelly />
-        <Cosmo className={styles.SceneContainerFooterCosmo} />
+        <TwitchChatBox messages={messages} />
+        <div className={styles.SceneContainerFooterDiscord}>
+          <FaDiscord />
+          <p>
+            <strong>Join the Discord</strong>
+            <span>spacejelly.dev/discord</span>
+          </p>
+        </div>
+        <div className={styles.SceneContainerFooterCosmo}>
+          <Cosmo />
+        </div>
       </div>
+      {!stale &&
+        current &&
+        toast({
+          position: 'top',
+          title: `${event.type} from ${event.from}`,
+          duration: ALERT_TIMER,
+          status: 'success',
+        })}
     </div>
   );
 }
---
+++ src/components/TwitchChatBox/TwitchChatBox.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Flex, Text } from '@chakra-ui/core';
+
+import styles from './TwitchChatBox.module.scss';
+
+const testMessages = [
+  {
+    user: 'colbyfayock',
+    text: 'asod naosdklg naldxkgn aldk nalskdn alsdk ngalsdknga lsdk'
+  },
+  {
+    user: 'colbyfayock',
+    text: 'two'
+  },
+  {
+    user: 'colbyfayock',
+    text: 'three'
+  },
+]
+
+export default function TwitchChatBox({ messages }) {
+  const hasMessages = Array.isArray(messages) && messages.length > 0;
+  return (
+    <div className={styles.TwitchChatBox}>
+      {hasMessages && (
+        <ul>
+          {messages.map((message, index) => {
+            return (
+              <li>
+                <strong>{message.user}</strong>
+                <span>{message.text}</span>
+              </li>
+            )
+          })}
+        </ul>
+      )}
+      {!hasMessages && (
+        <p className={styles.TwitchChatBoxJoin}>
+          Join the chat at twitch.tv/{process.env.CHANNEL}
+        </p>
+      )}
+    </div>
+  );
+}
